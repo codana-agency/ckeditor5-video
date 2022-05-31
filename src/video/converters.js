56a07fc440143406052e64be981d6b1bf4b8ae1f
Thomas Timmermans <timmeth@cronos.be>
2022-05-31 15:57:34 +0200

    fix conflict video image

--- src/video/converters.js
+++ src/video/converters.js
@@ -1,49 +1,65 @@
-import { first } from 'ckeditor5/src/utils';
+import { first } from "ckeditor5/src/utils";
 
-export function upcastVideoFigure( videoUtils ) {
-	return dispatcher => {
-		dispatcher.on( 'element:figure', converter );
-	};
+export function upcastVideoFigure(videoUtils) {
+  return (dispatcher) => {
+    dispatcher.on("element:figure", converter);
+  };
 
-	function converter( evt, data, conversionApi ) {
-		if ( !conversionApi.consumable.test( data.viewItem, { name: true, classes: 'video' } ) ) {
-			return;
-		}
+  function converter(evt, data, conversionApi) {
+    if (
+      !conversionApi.consumable.test(data.viewItem, {
+        name: true,
+        classes: "video",
+      })
+    ) {
+      return;
+    }
 
-		const viewVideo = videoUtils.findViewVideoElement( data.viewItem );
+    const viewVideo = videoUtils.findViewVideoElement(data.viewItem);
 
-		if ( !viewVideo || !viewVideo.hasAttribute( 'src' ) || !conversionApi.consumable.test( viewVideo, { name: true } ) ) {
-			return;
-		}
+    if (
+      !viewVideo ||
+      !viewVideo.hasAttribute("src") ||
+      !conversionApi.consumable.test(viewVideo, { name: true })
+    ) {
+      return;
+    }
 
-		const conversionResult = conversionApi.convertItem( viewVideo, data.modelCursor );
+    const conversionResult = conversionApi.convertItem(
+      viewVideo,
+      data.modelCursor
+    );
 
-		const modelVideo = first( conversionResult.modelRange.getItems() );
+    const modelVideo = first(conversionResult.modelRange.getItems());
 
-		if ( !modelVideo ) {
-			return;
-		}
+    if (!modelVideo) {
+      return;
+    }
 
-		conversionApi.convertChildren( data.viewItem, modelVideo );
+    conversionApi.convertChildren(data.viewItem, modelVideo);
 
-		conversionApi.updateConversionResult( modelVideo, data );
-	}
+    conversionApi.updateConversionResult(modelVideo, data);
+  }
 }
 
-export function downcastVideoAttribute( videoUtils, videoType, attributeKey ) {
-	return dispatcher => {
-		dispatcher.on( `attribute:${ attributeKey }:${ videoType }`, converter );
-	};
+export function downcastVideoAttribute(videoUtils, videoType, attributeKey) {
+  return (dispatcher) => {
+    dispatcher.on(`attribute:${attributeKey}:${videoType}`, converter);
+  };
 
-	function converter( evt, data, conversionApi ) {
-		if ( !conversionApi.consumable.consume( data.item, evt.name ) ) {
-			return;
-		}
+  function converter(evt, data, conversionApi) {
+    if (!conversionApi.consumable.consume(data.item, evt.name)) {
+      return;
+    }
 
-		const viewWriter = conversionApi.writer;
-		const element = conversionApi.mapper.toViewElement( data.item );
-		const video = videoUtils.findViewVideoElement( element );
+    const viewWriter = conversionApi.writer;
+    const element = conversionApi.mapper.toViewElement(data.item);
+    const video = videoUtils.findViewVideoElement(element);
 
-		viewWriter.setAttribute( data.attributeKey, data.attributeNewValue || '', video );
-	}
+    viewWriter.setAttribute(
+      data.attributeKey,
+      data.attributeNewValue || "",
+      video
+    );
+  }
 }
--- src/videostyle/converters.js
+++ src/videostyle/converters.js
@@ -1,57 +1,68 @@
-import { first } from 'ckeditor5/src/utils';
+import { first } from "ckeditor5/src/utils";
 
-export function modelToViewStyleAttribute( styles ) {
-    return ( evt, data, conversionApi ) => {
-        if ( !conversionApi.consumable.consume( data.item, evt.name ) ) {
-            return;
-        }
-
-        const newStyle = getStyleDefinitionByName( data.attributeNewValue, styles );
-        const oldStyle = getStyleDefinitionByName( data.attributeOldValue, styles );
-
-        const viewElement = conversionApi.mapper.toViewElement( data.item );
-        const viewWriter = conversionApi.writer;
-
-        if ( oldStyle ) {
-            viewWriter.removeClass( oldStyle.className, viewElement );
-        }
-
-        if ( newStyle ) {
-            viewWriter.addClass( newStyle.className, viewElement );
-        }
-    };
-}
-
-export function viewToModelStyleAttribute( styles ) {
-    const nonDefaultStyles = {
-        videoInline: styles.filter( style => !style.isDefault && style.modelElements.includes( 'videoInline' ) ),
-        videoBlock: styles.filter( style => !style.isDefault && style.modelElements.includes( 'videoBlock' ) )
-    };
-
-    return ( evt, data, conversionApi ) => {
-        if ( !data.modelRange ) {
-            return;
-        }
-
-        const viewElement = data.viewItem;
-        const modelVideoElement = first( data.modelRange.getItems() );
-
-        if ( !modelVideoElement ) {
-            return;
-        }
-
-        for ( const style of nonDefaultStyles[ modelVideoElement.name ] ) {
-            if ( conversionApi.consumable.consume( viewElement, { classes: style.className } ) ) {
-                conversionApi.writer.setAttribute( 'videoStyle', style.name, modelVideoElement );
-            }
-        }
-    };
-}
-
-function getStyleDefinitionByName( name, styles ) {
-    for ( const style of styles ) {
-        if ( style.name === name ) {
-            return style;
-        }
+export function modelToViewStyleAttribute(styles) {
+  return (evt, data, conversionApi) => {
+    if (!conversionApi.consumable.consume(data.item, evt.name)) {
+      return;
     }
+
+    const newStyle = getStyleDefinitionByName(data.attributeNewValue, styles);
+    const oldStyle = getStyleDefinitionByName(data.attributeOldValue, styles);
+
+    const viewElement = conversionApi.mapper.toViewElement(data.item);
+    const viewWriter = conversionApi.writer;
+
+    if (oldStyle) {
+      viewWriter.removeClass(oldStyle.className, viewElement);
+    }
+
+    if (newStyle) {
+      viewWriter.addClass(newStyle.className, viewElement);
+    }
+  };
+}
+
+export function viewToModelStyleAttribute(styles) {
+  const nonDefaultStyles = {
+    videoInline: styles.filter(
+      (style) => !style.isDefault && style.modelElements.includes("videoInline")
+    ),
+    videoBlock: styles.filter(
+      (style) => !style.isDefault && style.modelElements.includes("videoBlock")
+    ),
+  };
+
+  return (evt, data, conversionApi) => {
+    if (!data.modelRange) {
+      return;
+    }
+
+    const viewElement = data.viewItem;
+    const modelVideoElement = first(data.modelRange.getItems());
+
+    if (!modelVideoElement || modelVideoElement.name !== "videoBlock") {
+      return;
+    }
+    for (const style of nonDefaultStyles[modelVideoElement.name]) {
+      if (
+        conversionApi.consumable.consume(viewElement, {
+          classes: style.className,
+        })
+      ) {
+        conversionApi.writer.setAttribute(
+          "videoStyle",
+          style.name,
+          modelVideoElement
+        );
+      }
+    }
+  };
+}
+
+function getStyleDefinitionByName(name, styles) {
+  for (const style of styles) {
+    if (style.name === name) {
+      return style;
+    }
+  }
 }
